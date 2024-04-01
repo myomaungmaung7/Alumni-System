46d8bcb1c48ce5a62adc27b7b4884781423324cc
Yell Yint <yealyint1879@gmail.com>
2024-04-01 19:29:38 +0630

    First Commint

---
+++ src/main/java/iat/aui/IatAlumniSystemApplication.java
@@ -0,0 +1,13 @@
+package iat.aui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IatAlumniSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IatAlumniSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/iat/aui/IatAlumniSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package iat.aui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IatAlumniSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
