bff7345c5a240f95ab3c1d43ff95d581b812d608
Yell Yint <yealyint1879@gmail.com>
2024-04-01 21:55:18 +0630

    third commit

--- src/main/java/iat/aui/IatAlumniSystemApplication.java
+++ src/main/java/iat/aui/IatAlumniSystemApplication.java
@@ -10,6 +10,7 @@ public class IatAlumniSystemApplication {
 		SpringApplication.run(IatAlumniSystemApplication.class, args);
 		System.out.print("This is testing");
 		System.out.print("This is second testing");
+		System.out.print("This is third testing");
 	}
 
 }
