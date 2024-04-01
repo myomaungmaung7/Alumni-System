5d2c9ef29e1b6ef68f9b79e17d9d9669caa478bf
Yell Yint <yealyint1879@gmail.com>
2024-04-01 21:36:56 +0630

    final testing

--- src/main/java/iat/aui/IatAlumniSystemApplication.java
+++ src/main/java/iat/aui/IatAlumniSystemApplication.java
@@ -8,11 +8,8 @@ public class IatAlumniSystemApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(IatAlumniSystemApplication.class, args);
-<<<<<<< HEAD
 		System.out.print("This is testing");
-=======
-		System.out.print("Gimme a  break");
->>>>>>> branch 'master' of https://github.com/myomaungmaung7/Alumni-System.git
+		System.out.print("This is second testing");
 	}
 
 }
